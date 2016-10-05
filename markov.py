9dc9d2087782ae7980126dca3ccda74abee7dced
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-04 17:18:27 -0700

    Rewrote make_chains with .get and now it works.

--- markov.py
+++ markov.py
@@ -26,40 +26,22 @@ def make_chains(text_string):
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
 
-    split_into_lots_of_tiny_strings = text_string.split()
+    split_string = text_string.split()
 
     chains = {}
 
-    #key is a bi-gram tuple, value is a list of strings which can follow the tuple
+    # key is a bi-gram tuple, value is a list of strings which can follow the tuple
+    for index in range(len(split_string)-2):
 
-    # for index, word in enumerate(split_into_lots_of_tiny_strings):
-    #     print index, word
+        key = (split_string[index], 
+               split_string[index + 1])
 
-    #     while index < len(split_into_lots_of_tiny_strings):
+        value = split_string[index +2]
 
-    for index in range(len(split_into_lots_of_tiny_strings)-1):
-        key = (split_into_lots_of_tiny_strings[index], 
-               split_into_lots_of_tiny_strings[index + 1])
-        
-        second_gram = key[1]
-        print second_gram
+        chains[key] = chains.get(key, [])
+        chains[key].append(value)
 
-        value = []
-        
-        # find all instances of second_gram and store the words that follow in a list
-        for index, word in enumerate(split_into_lots_of_tiny_strings):
-            
-            if word == split_into_lots_of_tiny_strings[-2]:
-                break
-
-            # For all instances of second_gram, store the following word in value
-            elif word == second_gram:   
-                value.append(split_into_lots_of_tiny_strings[index + 2])
-
-            chains[key] = value
-
-        
-    print chains  
+    return chains
 
 
 
@@ -79,7 +61,6 @@ input_path = "green-eggs.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
-print input_text
 
 # Get a Markov chain
 chains = make_chains(input_text)
